884a39ee010f2ff2da37a433aae0d25bcdfc66e8
madhu-softwaredeveloper <141350241+madhu-softwaredeveloper@users.noreply.github.com>
2023-08-04 11:10:53 +0530

    Arithmatic file uploaded

---
+++ arithmatic.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+
+class arithmatic
+{
+	public static void main(String args[])
+	{
+		 int a=50,b=39;
+		 System.out.println("Addition :"+(a+b));
+		 System.out.println("Subtraction :"+(a-b));
+		 System.out.println("Multiplication :"+(a*b));
+		 System.out.println("Division :"+(a/b));
+		 System.out.println("Modulus :"+(a%b));
+	}
+}
