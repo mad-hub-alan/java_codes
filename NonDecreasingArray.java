def7e5fbde670d3482b4fb1f5a68b7e0418664e7
MadhuBalan M <141350241+mad-hub-alan@users.noreply.github.com>
2024-06-18 17:00:04 +0530

    Add files via upload
    This piece of code evaluates the given array is decresing or not

---
+++ NonDecreasingArray.java
@@ -0,0 +1,23 @@
+public class NonDecreasingArray {
+    static Boolean nonDecreasingArray(int[] arr) {
+
+
+        for(int i=1; i<arr.length; i++){
+
+            int temp = arr[i-1];
+
+            if(arr[i]<temp){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String args[]) {
+
+        assert (nonDecreasingArray(new int[] { 1, 2, 3 })) : "Expect true for array = {1, 2, 3}";
+        System.out.println("All test cases in main function passed");
+
+        System.out.println(nonDecreasingArray(new int[] {1,5,8,7}));
+    }
+}
