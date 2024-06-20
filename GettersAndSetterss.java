c48dc95da911fdd1a061926ce536bcab4d14bd6a
MadhuBalan M <141350241+mad-hub-alan@users.noreply.github.com>
2024-06-20 22:22:02 +0530

    Add files via upload
    Basic Code for Getters and Setters in Java

---
+++ GettersAndSetterss.java
@@ -0,0 +1,23 @@
+public class GettersAndSetterss {
+    public int beets;
+
+    public GettersAndSetterss() {
+        this.beets = 0;
+    }
+
+    public int GetBeets() {
+        return this.beets;
+    }
+
+    public void SetBeets(int beets) {
+        this.beets = beets;
+    }
+
+    public static void main(String args[]) {
+        GettersAndSetterss ob = new GettersAndSetterss();
+        ob.SetBeets(21);
+        assert ob.GetBeets() == 21 : "Setting beets = 21 then Getting beets";
+        System.out.println("All test cases in main function passed");
+
+    }
+}
