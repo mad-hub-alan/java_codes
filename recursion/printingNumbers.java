1dcb75151e5cf9891058d9602b4112541dd6f8cc
Madhu <madhuofficial02@gmail.com>
2024-11-06 20:42:57 +0530

    recursion folder added

---
+++ recursion/printingNumbers.java
@@ -0,0 +1,20 @@
+package recursion;
+
+public class printingNumbers {
+
+    private static void print(int i){
+        if(i == 100){   
+            return;         // Termination Condition
+        }
+        System.out.print(" "+i+" ");
+        print(i + 1);   // recursive statement
+    }
+
+
+
+    public static void main(String[] args){
+
+        print(0);
+
+    }
+}
