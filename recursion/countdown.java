bc62d2f8428a151acdea6948face833d5406abcb
Madhu <madhuofficial02@gmail.com>
2024-11-06 20:51:07 +0530

    countdown by recursion

---
+++ recursion/countdown.java
@@ -0,0 +1,16 @@
+package recursion;
+
+public class countdown {
+    
+    private static void countdown(int n){
+        if(n == -1){
+            return;
+        }
+        System.out.print(" \n"+ n +" ");
+        countdown(n -1);
+    }
+
+    public static void main(String[] args){
+        countdown(100);
+    }
+}
