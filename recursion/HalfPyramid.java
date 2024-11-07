48191e3c2f457ea4a6a2edfb08d9c34b6f2e2082
Madhu <madhuofficial02@gmail.com>
2024-11-07 22:05:07 +0530

    Half Pyramid By Recursive Call Program Added

---
+++ recursion/HalfPyramid.java
@@ -0,0 +1,22 @@
+package recursion;
+
+public class HalfPyramid {
+    
+    private static void halfPyramid(int p){
+        if(p == 0){
+            return;
+        }
+        String s = "";
+       
+        for(int i=0; i<p; i++){
+        System.out.print("*");
+       }
+
+       System.out.println(s);
+       halfPyramid(p - 1);
+    }
+
+    public static void main(String[] args){
+        halfPyramid(10);
+    }
+}
