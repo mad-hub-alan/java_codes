e52c714c995637b5f433af01af3f89d488485c6a
Madhu <madhuofficial02@gmail.com>
2024-11-16 15:07:41 +0530

    BinarySearch Program Added

---
+++ DSA/searching_algorithms/BinarySearchReverse.java
@@ -0,0 +1,33 @@
+package DSA.searching_algorithms;
+
+public class BinarySearchReverse {
+    
+    static int binarySearchReverse(int[] a, int n, int X){
+        int left = 0;
+        int right = n-1;
+
+        while(left <=right){
+            int mid = (left+right)/2;
+
+            if(a[mid] == X)
+                return mid;
+            else if(a[mid] < X)
+                right = mid - 1;
+            else
+                left = mid + 1;
+        }
+        return -1;
+    }
+
+
+    public static void main(String[] args){
+        int[] arr = {999,666,444,258,167,100,99,58,44,21,4};
+        
+        int n = arr.length;
+
+        int target = 444;
+
+        System.out.println(binarySearchReverse(arr,n,target));
+
+    }
+}
